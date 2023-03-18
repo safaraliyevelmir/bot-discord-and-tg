bbc732bd2e28f83a0e24ec5d1a2262e1784d59be
safaraliyevelmir <safaraliyevelmir@gmail.com>
2023-03-18 12:18:33 +0000

    hello

--- platformas/indeed.py
+++ platformas/indeed.py
@@ -16,12 +16,7 @@ class Indeed:
         return self.get_data(url)
 
     def get_data(self,url):
-        driver_options = webdriver.ChromeOptions()
-        driver_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        driver_options.add_argument("--headless")
-        driver_options.add_argument("--disable-dev-shm-usage")
-        driver_options.add_argument("--no-sandbox")
-        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=driver_options)
+        driver=webdriver.Chrome()
 
         driver.get(url)
         time.sleep(5)                        
