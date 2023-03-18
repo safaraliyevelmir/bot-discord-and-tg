3504bc18192bb592b5618a7e069707bfbcbaa47e
safaraliyevelmir <safaraliyevelmir@gmail.com>
2023-03-18 11:54:46 +0000

    chrome

--- platformas/remoteco.py
+++ platformas/remoteco.py
@@ -16,7 +16,12 @@ class Remote:
         return self.get_data(self.MAIN_URL + work_name)
     
     def get_data(self,url):
-        driver = webdriver.Chrome()
+        driver_options = webdriver.ChromeOptions()
+        driver_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        driver_options.add_argument("--headless")
+        driver_options.add_argument("--disable-dev-shm-usage")
+        driver_options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=driver_options)
         driver.get(url)
         time.sleep(2)
         links = driver.find_elements(By.CLASS_NAME,"stretched-link")
