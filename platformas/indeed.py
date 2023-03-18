7bad2fed47d527f158b433aaa6b5174e6d724086
safaraliyevelmir <safaraliyevelmir@gmail.com>
2023-03-18 11:42:26 +0000

    chrome-driver

--- platformas/indeed.py
+++ platformas/indeed.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import os
 
 # driver = webdriver.Firefox()
 #https://www.indeed.com/jobs?q=django&l=Remote&start=0
@@ -15,7 +16,13 @@ class Indeed:
         return self.get_data(url)
 
     def get_data(self,url):
-        driver = webdriver.Chrome()
+        driver_options = webdriver.ChromeOptions()
+        driver_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        driver_options.add_argument("--headless")
+        driver_options.add_argument("--disable-dev-shm-usage")
+        driver_options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=driver_options)
+
         driver.get(url)
         time.sleep(5)                        
         elements = driver.find_elements(By.XPATH,'/html/body/main/div/div[1]/div/div/div[5]/div[1]/div[5]/div/ul/li/div/div[1]/div/div[1]/div/table[1]/tbody/tr/td/div[1]/h2/a')
