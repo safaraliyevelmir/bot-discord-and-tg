4a6179f029b51e0d3de48363ad4c0f1240242a5d
safaraliyevelmir <safaraliyevelmir@gmail.com>
2023-04-01 16:36:20 +0100

    selenium test

--- platformas/indeed.py
+++ platformas/indeed.py
@@ -18,7 +18,11 @@ class Indeed:
     
     #get_data  dataları gətirir
     def get_data(self,url):
-        driver=webdriver.Chrome()
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
         driver.get(url)
         time.sleep(5)                        
         elements = driver.find_elements(By.XPATH,'/html/body/main/div/div[1]/div/div/div[5]/div[1]/div[5]/div/ul/li/div/div[1]/div/div[1]/div/table[1]/tbody/tr/td/div[1]/h2/a')
